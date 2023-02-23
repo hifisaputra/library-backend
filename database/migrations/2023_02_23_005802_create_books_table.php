14e5043a0830983f34a05ec8c053f8e39d2533fc
Hifi Saputra <hifisaputra1@gmail.com>
2023-02-23 08:06:04 +0700

    adding models and migration

---
+++ database/migrations/2023_02_23_005802_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('cover');
+            $table->integer('year');
+            $table->string('publisher');
+            $table->string('author');
+            $table->timestamps();
+
+            $table->bigInteger('shelf_id')->unsigned()->index();
+            $table->foreign('shelf_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
